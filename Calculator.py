902f142117ada14d30c2aeac4a2389588b920793
hmztj <hmztj1@gmail.com>
2018-12-14 18:42:46 +0000

    initial commit

---
+++ Calculator.py
@@ -0,0 +1,24 @@
+number1_str = input(" First number: ")
+number1 = int(number1_str)
+
+number2_str = input("Second number: ")
+number2 = int(number2_str)
+
+operation = input("Operation [+, -, *, /]: ")
+
+if operation == "+":
+    combination = number1 + number2
+elif operation == "-":
+    combination = number1 - number2
+elif operation == "*":
+    combination = number1 * number2
+elif operation == "/":
+    combination = number1 / number2
+else:
+    combination = "ERROR ... '" + operation + "' unrecognised"
+
+combination_str = str(combination)
+print("Answer: " + combination_str)
+
+print()
+input("Press return to continue ...")
